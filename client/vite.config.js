fc18f9b789f68b802733bc7e5f8c338ea9cc376c
govindpaswan <paswangovind680@gmail.com>
2026-04-03 11:52:31 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": { target: "http://localhost:5000", changeOrigin: true },
+      "/api": { target: "https://learnify-server-7stm.onrender.com", changeOrigin: true },
     },
   },
 });
